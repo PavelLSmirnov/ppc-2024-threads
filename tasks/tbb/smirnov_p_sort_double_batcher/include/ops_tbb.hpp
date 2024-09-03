770403b24936915b2ce8e8ea8769c4ca6e89e914
PavelLSmirnov <pavel1201s@yandex.ru>
2024-09-03 21:34:16 +0300

    feat(tbb): complete lab 3

--- tasks/tbb/smirnov_p_sort_double_batcher/include/ops_tbb.hpp
+++ tasks/tbb/smirnov_p_sort_double_batcher/include/ops_tbb.hpp
@@ -1,7 +1,7 @@
 // Copyright 2024 Smirnov Pavel
 #pragma once
 
-#include <omp.h>
+#include <tbb/tbb.h>
 
 #include <algorithm>
 #include <cstdint>
@@ -10,8 +10,7 @@
 
 #include "core/task/include/task.hpp"
 
-namespace SmirnovPavelOmp {
-
+namespace SmirnovPavelTbb {
 class RadixSortDoubleBatcherSequential : public ppc::core::Task {
  public:
   explicit RadixSortDoubleBatcherSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
@@ -27,9 +26,9 @@ class RadixSortDoubleBatcherSequential : public ppc::core::Task {
   std::vector<double> res;
 };
 
-class RadixSortDoubleBatcherOmpParallel : public ppc::core::Task {
+class RadixSortDoubleBatcherTbbParallel : public ppc::core::Task {
  public:
-  explicit RadixSortDoubleBatcherOmpParallel(std::shared_ptr<ppc::core::TaskData> taskData_)
+  explicit RadixSortDoubleBatcherTbbParallel(std::shared_ptr<ppc::core::TaskData> taskData_)
       : Task(std::move(taskData_)) {}
 
   bool pre_processing() override;
@@ -44,11 +43,11 @@ class RadixSortDoubleBatcherOmpParallel : public ppc::core::Task {
 
 std::vector<double> batchersMergeSeq(std::vector<std::vector<double>>& subvectors);
 void partSortSeq(std::vector<std::vector<double>>& parts, std::vector<double>& side);
-std::vector<double> radixSortBatcherSeq(std::vector<double> vec);
+std::vector<double> radixSortBatcherSeq(std::vector<double> v);
 
-std::vector<double> batchersMergeOmp(std::vector<std::vector<double>>& subvectors);
-void partSortOmp(std::vector<std::vector<double>>& parts, std::vector<double>& side);
-std::vector<double> radixSortBatcherOmp(std::vector<double> vec);
+std::vector<double> batchersMergeTbb(std::vector<std::vector<double>>& subvectors);
+void partSortTbb(std::vector<std::vector<double>>& parts, std::vector<double>& side);
+std::vector<double> radixSortBatcherTbb(std::vector<double> v);
 
 std::vector<double> randomVector(int sizeVec, double minValue, double maxValue);
-}  // namespace SmirnovPavelOmp
+}  // namespace SmirnovPavelTbb
